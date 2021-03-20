c8c5554ef19fe2adadaaabdc48bc36f925d03bc9
Maksim Gazimzyanov <gazmaksimr@yandex.ru>
2021-03-20 17:24:13 +0300

    Initial Commit

---
+++ VeloToApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeloToApp
+//
+//  Created by Максим Газимзянов on 20.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
